b0ac8f21b67deae6623853f6102a942ad9d9caa3
Dan Jecu <60711891+DanJecu@users.noreply.github.com>
2024-05-15 07:03:49 +0200

    update linting

--- .config/nvim/lua/config/plugins/linting.lua
+++ .config/nvim/lua/config/plugins/linting.lua
@@ -13,6 +13,12 @@ return {
 			python = { "pylint" },
 		}
 
+		local eslint = lint.linters.eslint_d
+
+		eslint.args = {
+			"--no-warn-ignored",
+		}
+
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
 		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
