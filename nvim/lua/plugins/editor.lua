6143c331d2d665fb2c4c825afd760062ebb91787
DanJecu <60711891+DanJecu@users.noreply.github.com>
2024-12-29 17:33:56 +0100

    exclude node_modules from fzf

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -1,1 +1,11 @@
-return {}
+return {
+  {
+    "ibhagwan/fzf-lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      files = {
+        cmd = "fd --type f --exclude node_modules --exclude .nx",
+      },
+    },
+  },
+}
