3b5d2b54e7f82b3c70bc04f5ea1886adbb5803ab
DanJecu <60711891+DanJecu@users.noreply.github.com>
2025-03-10 19:17:11 +0100

    use lazyvim

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,88 @@
+-- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
+--
+-- In your plugin files, you can:
+-- * add extra plugins
+-- * disable/enabled LazyVim plugins
+-- * override the configuration of LazyVim plugins
 return {
+
+  { "edeneast/nightfox.nvim" },
   {
-    'edeneast/nightfox.nvim',
-    lazy = false,
-    priority = 1000,
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "nordfox",
+    },
+  },
+
+  -- add pyright to lspconfig
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        -- pyright will be automatically installed with mason and loaded with lspconfig
+        pyright = {},
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      vim.cmd 'colorscheme nordfox'
+      local git_blame = require("gitblame")
+      require("lualine").setup({
+        options = {
+          globalstatus = true,
+        },
+        sections = {
+          lualine_x = {
+            { git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available },
+          },
+        },
+      })
     end,
+  },
+  {
+    "f-person/git-blame.nvim",
+    lazy = false,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      vim.g.gitblame_display_virtual_text = 0
+      vim.g.gitblame_message_template = "<author> • <date>"
+      vim.g.gitblame_date_format = "%r" -- relative time format
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shellcheck",
+        "shfmt",
+      },
+    },
   },
 }
