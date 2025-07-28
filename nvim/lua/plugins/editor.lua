a0d891c74103e4a29a6b44737119f65eab134e90
DanJecu <danjecu@outlook.com>
2025-07-28 19:07:20 +0200

    add plugins

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -422,16 +422,46 @@ return {
 			end, { desc = "TODOs in current file (Quickfix)" })
 		end,
 	},
+	{
+		"nvim-tree/nvim-tree.lua",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			vim.g.loaded_netrw = 1
+			vim.g.loaded_netrwPlugin = 1
 
-	-- {
-	-- 	"supermaven-inc/supermaven-nvim",
-	-- 	config = function()
-	-- 		require("supermaven-nvim").setup({
-	-- 			disable_inline_completion = true,
-	-- 		})
-	--
-	-- 		-- Keybindings
-	-- 		vim.keymap.set("n", "<leader>sm", "<cmd>SupermavenToggle<cr>", { desc = "Toggle Supermaven" })
-	-- 	end,
-	-- },
+			require("nvim-tree").setup({
+				update_focused_file = {
+					enable = true,
+					update_root = true,
+				},
+				view = {
+					width = 30,
+					side = "left",
+				},
+				renderer = {
+					group_empty = true,
+				},
+				filters = {
+					dotfiles = false,
+					git_ignored = false,
+				},
+			})
+
+			-- Use FindFileToggle to open at current file
+			vim.keymap.set("n", "<leader>e", ":NvimTreeFindFileToggle<CR>", { desc = "Toggle NvimTree (focused)" })
+		end,
+	},
+	{
+		"supermaven-inc/supermaven-nvim",
+		config = function()
+			require("supermaven-nvim").setup({
+				disable_inline_completion = true,
+			})
+
+			-- Keybindings
+			vim.keymap.set("n", "<leader>sm", "<cmd>SupermavenToggle<cr>", { desc = "Toggle Supermaven" })
+		end,
+	},
 }
