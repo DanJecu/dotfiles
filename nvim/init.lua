62ca50f97055b95d98c3b579caca5146815e12bd
DanJecu <60711891+DanJecu@users.noreply.github.com>
2025-11-15 17:38:15 +0100

    cleanup

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,24 +1,2 @@
-function EditLineFromLazygit(file_path, line)
-	local path = vim.fn.expand("%:p")
-	if path == file_path then
-		vim.cmd(tostring(line))
-	else
-		vim.cmd("e " .. file_path)
-		vim.cmd(tostring(line))
-	end
-end
-
-function EditFromLazygit(file_path)
-	local path = vim.fn.expand("%:p")
-	if path == file_path then
-		return
-	else
-		vim.cmd("e " .. file_path)
-	end
-end
-
-require("config.options")
-require("config.keymaps")
-require("config.autocmds")
-
+-- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
---
+++ nvim/lua/plugins/extras.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "Mofiqul/vscode.nvim",
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "vscode",
+      scroll = { enabled = false },
+    },
+  },
+}
