43bf6cf5eba7fa01115235c8de0e13656cd8c06d
DanJecu <60711891+DanJecu@users.noreply.github.com>
2025-12-07 19:51:53 +0100

    basics and colorscheme

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,23 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+-- Copy relative path and line numeber on cursor
+vim.keymap.set("n", "<leader>cp", function()
+  local file = vim.fn.expand("%:p")
+  local home = vim.fn.expand("~")
+  local relative_path = file:gsub("^" .. vim.pesc(home) .. "/", "~/")
+  local line = vim.fn.line(".")
+  local result = relative_path .. ":" .. line
+  vim.fn.setreg("+", result)
+  print("Copied: " .. result)
+end, { desc = "Copy file path with line number (from ~)" })
+
+-- Open explorer
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Open Explorer" })
+
+-- Move selected lines
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Select all
+vim.keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- Open commit url
+vim.keymap.set("n", "<leader>gc", ":GitBlameOpenCommitURL<CR>", { desc = "[G]it Open [C]ommit URL" })
---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "sainnhe/sonokai",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      vim.cmd.colorscheme("sonokai")
+    end,
+  },
+}
