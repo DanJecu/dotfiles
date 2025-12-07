c03ed7eed8531cf1d4f0ce86779223e44b3dd9c0
DanJecu <60711891+DanJecu@users.noreply.github.com>
2025-12-07 09:49:53 +0100

    fix theme for snack picker

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,21 @@
-return  { 
-    'loctvl842/monokai-pro.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    config = function()
-      require('monokai-pro').setup {
-        styles = {
-        },
-	filter='spectrum'
-      }
-      vim.cmd.colorscheme 'monokai-pro'
-    end,
-  }
+return {
+  'loctvl842/monokai-pro.nvim',
+  priority = 1000, -- Make sure to load this before all the other start plugins.
+  config = function()
+    require('monokai-pro').setup {
+      styles = {},
+      filter = 'spectrum',
+      override = function(c)
+        local hp = require 'monokai-pro.color_helper'
+        local common_fg = hp.lighten(c.sideBar.foreground, 30)
+        return {
+          SnacksPicker = { bg = c.editor.background, fg = common_fg },
+          SnacksPickerBorder = { bg = c.editor.background, fg = c.tab.unfocusedActiveBorder },
+          SnacksPickerTree = { fg = c.editorLineNumber.foreground },
+          NonText = { fg = c.base.dimmed3 }, -- not sure if this should be broken into all hl groups importing NonText
+        }
+      end,
+    }
+    vim.cmd.colorscheme 'monokai-pro'
+  end,
+}
--- nvim/lua/plugins/mini-files.lua
+++ nvim/lua/plugins/mini-files.lua
@@ -1,25 +1,25 @@
 return {
-		-- Explorer
-		"echasnovski/mini.files",
-		version = "*",
-		config = function()
-			local MiniFiles = require("mini.files")
-			MiniFiles.setup({
-				options = { use_as_default_explorer = true },
-content = {
-    filter = function(name, _)
-      return true
-    end,
-  },
-			})
+  -- Explorer
+  'echasnovski/mini.files',
+  version = '*',
+  config = function()
+    local MiniFiles = require 'mini.files'
+    MiniFiles.setup {
+      options = { use_as_default_explorer = true },
+      content = {
+        filter = function(name, _)
+          return true
+        end,
+      },
+    }
 
-			vim.keymap.set("n", "<leader>pe", function()
-				MiniFiles.open(vim.api.nvim_buf_get_name(0), false)
-				MiniFiles.reveal_cwd()
-			end, { desc = "Open MiniFiles at current file's directory" })
+    vim.keymap.set('n', '<leader>pe', function()
+      MiniFiles.open(vim.api.nvim_buf_get_name(0), false)
+      MiniFiles.reveal_cwd()
+    end, { desc = "Open MiniFiles at current file's directory" })
 
-			vim.keymap.set("n", "<leader>pE", function()
-				require("mini.files").open()
-			end, { desc = "Open MiniFiles" })
-		end,
-	}
+    vim.keymap.set('n', '<leader>pE', function()
+      require('mini.files').open()
+    end, { desc = 'Open MiniFiles' })
+  end,
+}
