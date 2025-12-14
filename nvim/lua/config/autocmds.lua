a6babcb32c1caf5fa228882f21d84fb764ae1555
DanJecu <60711891+DanJecu@users.noreply.github.com>
2025-12-14 17:52:18 +0100

    add lazygit

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,29 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank()
 	end,
 })
+
+function EditLineFromLazygit(file_path, line)
+	local path = vim.fn.expand("%:p")
+	if path == file_path then
+		vim.cmd(tostring(line))
+	else
+		vim.cmd("e " .. file_path)
+		vim.cmd(tostring(line))
+	end
+end
+
+function EditFromLazygit(file_path)
+	local path = vim.fn.expand("%:p")
+	if path == file_path then
+		return
+	else
+		vim.cmd("e " .. file_path)
+	end
+end
+
+-- Lazygit
+vim.g.lazygit_use_custom_config_file_path = 1
+vim.g.lazygit_config_file_path = {
+	-- vim.fn.expand("~/.config/lazygit/config.yml"),
+	vim.fn.expand("~/.config/lazygit/config.nvim.yml"),
+}
